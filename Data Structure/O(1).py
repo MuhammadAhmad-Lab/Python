fc095a8a42e4f00a04173fead54259432c0cec34
MuhamamdAhmad-Lab <syedahmedmuhammad697@gmail.com>
2025-01-19 18:01:40 +0500

    O(1).py

---
+++ Data Structure/O(1).py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iterations=0
+    print("The number entered by the user is ",n)
+    iterations+=1
+    print("Total Iterations done by the code is ",iterations ,"\n")
+    
+print(10)
+print(20)
+print("\n With any 'n' the time taken wont change")
