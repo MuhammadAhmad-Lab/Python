769c350231eae47c285548b9ec15140ff1307723
MuhamamdAhmad-Lab <syedahmedmuhammad697@gmail.com>
2025-02-23 13:44:15 +0500

    Bitwise.py

---
+++ Data Structure/Implement_Bitwise.py
@@ -0,0 +1,18 @@
+num1 = 10
+num2 = 4
+
+print("Num1 & Num2= ", num1, num2)
+
+print("/nNum1 | Num2 =", num1 | num2)
+
+print("/n~Num1 =", ~num1)
+
+print("/nNum1 ^ Num2 =", num1 ^ num2)
+
+num1 = 10
+num2 = 4
+print("/nNum1 << 1 =", num1 >> 1)
+print("/nNum1 >> 2 =", num1 >> 2)
+
+print("/nNum1 << 1 =", num1 << 1)
+print("/nNum1 << 2 =", num1 << 2)
