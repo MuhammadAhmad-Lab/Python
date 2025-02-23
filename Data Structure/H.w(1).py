4d1c416a45df81fd08db78f2782eed7eea8e4f52
MuhamamdAhmad-Lab <syedahmedmuhammad697@gmail.com>
2025-02-23 14:00:10 +0500

    H.w(1).py

---
+++ Data Structure/H.w(1).py
@@ -0,0 +1,18 @@
+num1 = 21
+num2 = 7
+
+print("Num1 & Num2= ", num1, num2)
+
+print("/nNum1 | Num2 =", num1 | num2)
+
+print("/n~Num1 =", ~num1)
+
+print("/nNum1 ^ Num2 =", num1 ^ num2)
+
+num1 = 10
+num2 = 4
+print("/nNum1 >> 1 =", num1 >> 1)
+print("/nNum2 >> 2 =", num2 >> 2)
+
+print("/nNum1 << 1 =", num1 << 1)
+print("/nNum2 << 2 =", num2 << 2)
